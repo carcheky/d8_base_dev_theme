8078113e9f462fdd52913ceee85f3ae596f1d04a
carcheky <carcheky@gmail.com>
2019-05-23 13:39:30 +0200

    script to generate new themes

--- themerepo/generate-subtheme.sh
+++ themerepo/generate-subtheme.sh
@@ -15,15 +15,14 @@ if [ $1 ] ; then
   THEME_NAME=${1}
 fi
 
-rm -fr ../${THEME_NAME}
 cp -fr ${ORIGINAL_DIR} ../${THEME_NAME}
 
 NEW_DIR=$(cd ../${THEME_NAME}; pwd)
 # echo ${ORIGINAL_DIR}
 echo "===================================================================="
 echo "===================================================================="
-echo "=> OLD theme ${ORIGINAL_DIR}"
-echo "=> NEW theme ${NEW_DIR}"
+# echo "=> OLD theme ${ORIGINAL_DIR}"
+echo "=> NEW theme in ${NEW_DIR}"
 echo "===================================================================="
 echo "===================================================================="
 
@@ -32,7 +31,9 @@ rename "s/^themerepo/$THEME_NAME/" themerepo*
 cd ${NEW_DIR}/config/install/
 rename "s/^themerepo/$THEME_NAME/" themerepo*
 cd ${NEW_DIR}
-ls -Rla | grep $THEME_NAME
+# ls -Rla | grep $THEME_NAME
 
 find ${NEW_DIR} -type f -print0 | xargs -0 sed -i "s/themerepo/$THEME_NAME/g"
-find ${NEW_DIR} -type f -exec cat {} \; | grep $THEME_NAME
+# find ${NEW_DIR} -type f -exec cat {} \; | grep $THEME_NAME
+
+
