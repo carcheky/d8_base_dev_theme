f55e380dd5836639a5539b3fe21a900d871e93af
carcheky <carcheky@gmail.com>
2019-05-23 13:43:16 +0200

    Update generate-subtheme.sh

--- themerepo/generate-subtheme.sh
+++ themerepo/generate-subtheme.sh
@@ -2,7 +2,27 @@
 # vars
 ORIGINAL_DIR=$(pwd);
 # functions
+generate_theme(){
 
+  # echo ${ORIGINAL_DIR}
+  echo "===================================================================="
+  echo "===================================================================="
+  # echo "=> OLD theme ${ORIGINAL_DIR}"
+  echo "=> NEW theme in ${NEW_DIR}"
+  echo "===================================================================="
+  echo "===================================================================="
+
+  cd ${NEW_DIR}
+  rename "s/^themerepo/$THEME_NAME/" themerepo*
+  cd ${NEW_DIR}/config/install/
+  rename "s/^themerepo/$THEME_NAME/" themerepo*
+  cd ${NEW_DIR}
+  # ls -Rla | grep $THEME_NAME
+
+  find ${NEW_DIR} -type f -print0 | xargs -0 sed -i "s/themerepo/$THEME_NAME/g"
+  # find ${NEW_DIR} -type f -exec cat {} \; | grep $THEME_NAME
+
+}
 # logic
 
 if [ ! $1 ] ; then
@@ -14,26 +34,14 @@ fi
 if [ $1 ] ; then
   THEME_NAME=${1}
 fi
-
-cp -fr ${ORIGINAL_DIR} ../${THEME_NAME}
-
-NEW_DIR=$(cd ../${THEME_NAME}; pwd)
-# echo ${ORIGINAL_DIR}
-echo "===================================================================="
-echo "===================================================================="
-# echo "=> OLD theme ${ORIGINAL_DIR}"
-echo "=> NEW theme in ${NEW_DIR}"
-echo "===================================================================="
-echo "===================================================================="
-
-cd ${NEW_DIR}
-rename "s/^themerepo/$THEME_NAME/" themerepo*
-cd ${NEW_DIR}/config/install/
-rename "s/^themerepo/$THEME_NAME/" themerepo*
-cd ${NEW_DIR}
-# ls -Rla | grep $THEME_NAME
-
-find ${NEW_DIR} -type f -print0 | xargs -0 sed -i "s/themerepo/$THEME_NAME/g"
-# find ${NEW_DIR} -type f -exec cat {} \; | grep $THEME_NAME
+if [ -d ../${THEME_NAME} ]; then
+  echo "=> ya existe ${THEME_NAME}"
+fi
+if [ ! -d ../${THEME_NAME} ]; then
+  echo "=> creando ${THEME_NAME}"
+  cp -fr ${ORIGINAL_DIR} ../${THEME_NAME}
+  NEW_DIR=$(cd ../${THEME_NAME}; pwd)
+  generate_theme
+fi
 
 
