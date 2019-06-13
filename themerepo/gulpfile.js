836224ae1b0345b7e5231ee59ae1cd16de4590e3
carcheky <carcheky@gmail.com>
2019-06-13 16:19:31 +0200

    Update gulpfile.js

--- themerepo/gulpfile.js
+++ themerepo/gulpfile.js
@@ -6,50 +6,55 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var gulpautoprefixer = require("gulp-autoprefixer");
 var cssnano = require("cssnano");
 var sourcemaps = require("gulp-sourcemaps");
 var browserSync = require("browser-sync").create();
 var paths = {
-    styles: {
-        src: "./scss/**/*.scss",
-        dest: "./css",
-        reload: "index.html",
-    }
+  styles: {
+    src: "./scss/**/*.scss",
+    dest: "./css",
+    reload: "index.html",
+  }
 };
 
 
 // FUNCTION style
 function style() {
-    return (
-        gulp
-            .src(paths.styles.src)
-            .pipe(sourcemaps.init())
-            .pipe(sass())
-            .on("error", sass.logError)
-            // .pipe(postcss([autoprefixer(), cssnano()]))
-            .pipe(postcss([autoprefixer()]))
-            // .pipe(gulpautoprefixer())
-            // .pipe(sourcemaps.write())
-            .pipe(gulp.dest(paths.styles.dest))
-            .pipe(browserSync.stream())
-    );
+  return (
+    gulp
+      .src(paths.styles.src)
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .on("error", sass.logError)
+      // .pipe(postcss([autoprefixer(), cssnano()]))
+      .pipe(postcss([autoprefixer()]))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(paths.styles.dest))
+      .pipe(browserSync.stream())
+  );
 }
-exports.style = style;
+
+
+// FUNCTION reload
+function reload() {
+  browserSync.reload();
+}
 
 
 // FUNCTION watch
-function reload() {
-    browserSync.reload();
-}
 function watch() {
-    browserSync.init({
-        // server: {
-        //     baseDir: "./"
-        // },
-        proxy: "http://centrodeestudiosgarrigues"
-    });
-    gulp.watch(paths.styles.src, style)
-    gulp.watch(paths.styles.reload, reload);
+  browserSync.init({
+    // server: {
+    //     baseDir: "./"
+    // },
+    proxy: "http://centrodeestudiosgarrigues"
+  });
+  gulp.watch(paths.styles.src, style)
+  gulp.watch(paths.styles.reload, reload);
 }
+
+
+// TASKS EXPORT
+exports.style = style;
 exports.watch = watch
+exports.default = watch;
