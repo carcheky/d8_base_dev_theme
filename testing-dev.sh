2267f2cc9d10c85f438fdd014f9f520269ea461a
carcheky <carcheky@gmail.com>
2019-10-23 22:21:55 +0200

    testing-dev:

--- testing-dev.sh
+++ testing-dev.sh
@@ -4,7 +4,7 @@
 # composer require drupal/admin_toolbar drupal/config_direct_save drupal/layout_section_classes drupal/layout_builder_styles
 # composer update --with-dependencies
 
-# # AUTOINSTALACIÓN ES
+# AUTOINSTALACIÓN ES
 # drush si druparcheky --account-pass=admin --site-name=devmodules --locale=es -yvvv
 
 # AUTOINSTALACIÓN EN
@@ -28,15 +28,42 @@
 
 # drush -y en bartik
 # drush -y config-set system.theme default bartik
-# drush -y pmu ilutheme
-# rm -fr ../ilutheme
-# drush cr
-# bash generate-subtheme.sh ilutheme
-# drush -y en ilutheme
-# drush -y config-set system.theme default ilutheme
-# drush -y en druparcheky_theme
-# drush -y config-set system.theme default druparcheky_theme
-# drush -y pmu bartik
+# # drush -y pmu ilutheme
+# # rm -fr ../ilutheme
+# # drush cr
+# # bash generate-subtheme.sh ilutheme
+# # drush -y en ilutheme
+# # drush -y config-set system.theme default ilutheme
+# drush -y pmu druparcheky_theme
+
+
+zero_install_es(){
+sudo rm -fr /mnt/c/wsl/sites/devmodules/web/vendor
+sudo rm -fr /mnt/c/wsl/sites/devmodules/web/core
+sudo rm -fr /mnt/c/wsl/sites/devmodules/web/composer.lock
+
+composer require drupal/admin_toolbar drupal/config_direct_save drupal/config_split drupal/layout_section_classes drupal/layout_builder_styles
+composer update --with-dependencies
+
+drush si druparcheky --account-pass=admin --site-name=devmodules --locale=es -yvvv
+drush en admin_toolbar layout_builder layout_discovery config_direct_save layout_section_classes layout_builder_styles -y
+drush -y en druparcheky_theme
+drush -y config-set system.theme default druparcheky_theme
+drush -y pmu bartik
+drush cron
+}
+
+
+
+
+
+
+
+
+
+
+
+# zero_install_es
 drush cr
 
 cmd.exe /C start http://devmodules.local
