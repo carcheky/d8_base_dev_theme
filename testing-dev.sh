870219b18c334b8065877669cce9f91772de547b
carcheky <carcheky@gmail.com>
2019-10-24 13:09:58 +0200

    testing-dev:

--- testing-dev.sh
+++ testing-dev.sh
@@ -24,26 +24,31 @@
 # # drush -y config-set system.theme default ilutheme
 # drush -y pmu druparcheky_theme
 
+#! FUNCTIONS
+
 composer_require() {
   sudo rm -fr /mnt/c/wsl/sites/devmodules/web/vendor
   sudo rm -fr /mnt/c/wsl/sites/devmodules/web/core
   sudo rm -fr /mnt/c/wsl/sites/devmodules/web/*/contrib
   sudo rm -fr /mnt/c/wsl/sites/devmodules/web/composer.lock
-
   composer require drupal/admin_toolbar drupal/config_direct_save drupal/config_split drupal/layout_section_classes drupal/layout_builder_styles drupal/module_filter drupal/fontawesome drupal/devel
   composer update --with-dependencies
 }
+
 zero_install_en() {
   drush -y si druparcheky --account-pass=admin --site-name=devmodules --locale=en
 }
+
 zero_install_es() {
   drush -y si druparcheky --account-pass=admin --site-name=devmodules --locale=es
 }
+
 enable_modules() {
   drush -y en admin_toolbar devel module_filter fontawesome
   drush -y en config_direct_save config_split
   drush -y en layout_section_classes layout_builder_styles
 }
+
 enable_bartik() {
   drush -y en bartik
   drush -y config-set system.theme default bartik
@@ -63,19 +68,15 @@ enable_druparcheky_theme() {
   drush -y pmu ilutheme bartik
 }
 
+#! LOGIC
 # composer_require
-
 # zero_install_en
 zero_install_es
-
 enable_modules
-
 enable_bartik
 enable_druparcheky_theme
 # enable_ilutheme
-
 drush cr
 drush cron 2>/dev/null
-
 cmd.exe /C start http://devmodules.local
 echo "listo."
