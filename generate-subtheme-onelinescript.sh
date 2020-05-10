4776973b2e9daf06ab24fb8388992c0cfca44001
carcheky <carcheky@users.noreply.github.com>
2020-05-10 16:01:23 +0200

    Update generate-subtheme-onelinescript.sh

--- generate-subtheme-onelinescript.sh
+++ generate-subtheme-onelinescript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/carcheky/druparcheky_theme.git druparcheky_tmp
-rm -fr druparcheky_tmp/.git
-cd druparcheky_tmp
+git clone https://github.com/carcheky/druparcheky_theme.git druparcheky_theme
+rm -fr druparcheky_theme/.git
+cp -fr druparcheky_theme/druparcheky_subtheme .
+cd druparcheky_subtheme
 bash generate-subtheme.sh
 cd ..
-rm -fr druparcheky_tmp
+rm -fr druparcheky_subtheme
 
