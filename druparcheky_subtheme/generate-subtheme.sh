a13a88fc693cae799dc5b3330439ff9524a83eb2
carcheky <carcheky@gmail.com>
2020-06-03 19:19:09 +0200

    fixed script

--- druparcheky_subtheme/generate-subtheme.sh
+++ druparcheky_subtheme/generate-subtheme.sh
@@ -3,59 +3,42 @@
 ORIGINAL_DIR=$(pwd)
 sudo apt install rename -y
 sudo apt autoremove
-# functions
-generate_theme() {
 
-  # echo ${ORIGINAL_DIR}
-  echo "===================================================================="
-  echo "===================================================================="
-  # echo "=> OLD theme ${ORIGINAL_DIR}"
-  echo "=> NEW theme in ${NEW_DIR}"
-  echo "===================================================================="
-  echo "===================================================================="
 
-  cd ${NEW_DIR}
-  sudo rm -fr .git
-  rename "s/^druparcheky_theme/$THEME_NAME/" druparcheky_theme*
-  mv $THEME_NAME.info.yml.example $THEME_NAME.info.yml
-
-  # cd ${NEW_DIR}/config/install/
-  # rename "s/^druparcheky_theme/$THEME_NAME/" druparcheky_theme*
-
-  # cd ${NEW_DIR}
-  # ls -Rla | grep $THEME_NAME
-
-  find ${NEW_DIR} -type f -print0 | xargs -0 sed -i "s/druparcheky_theme/$THEME_NAME/g"
-  # find ${NEW_DIR} -type f -exec cat {} \; | grep $THEME_NAME
-
-}
 # logic
 
 if [ ! $1 ]; then
   echo "nombre del tema:"
   read THEME_NAME
-  THEME_NAME=${THEME_NAME}
+  # THEME_NAME=${THEME_NAME}
 fi
 
 if [ $1 ]; then
   THEME_NAME=${1}
-fi
-if [ ! -d ../../../custom/${THEME_NAME} ]; then
-  mkdir ../../../custom
+  NEW_DIR=$(drush dd)/themes/custom/${THEME_NAME}
+  echo "===================================================================="
+  echo "===================================================================="
+  echo "=> OLD theme ${ORIGINAL_DIR}"
+  echo "=> NEW theme in ${NEW_DIR}"
+  echo "===================================================================="
+  echo "===================================================================="
 fi
 
-if [ -d ../../../custom/${THEME_NAME} ]; then
-  echo "=> ya existe ${THEME_NAME}, borrando..."
-  rm -fr ../../../custom/${THEME_NAME}
+if [ ! -d $(drush dd)/themes/custom ]; then
+  mkdir -p $(drush dd)/themes/custom
 fi
-if [ ! -d ../../../custom/${THEME_NAME} ]; then
+
+if [ -d ${NEW_DIR} ]; then
+  echo "=> ya existe ${THEME_NAME}, cancelando"
+  exit
+fi
+if [ ! -d ${NEW_DIR} ]; then
   echo "=> creando ${THEME_NAME}"
-  cp -fr ${ORIGINAL_DIR} ../../../custom/${THEME_NAME}
-  NEW_DIR=$(
-    cd ../../../custom/${THEME_NAME}
-    rm generate-subtheme.sh
-    pwd
-  )
-  generate_theme
+  cp -fr ${ORIGINAL_DIR} ${NEW_DIR}
+  cd ${NEW_DIR}
+  sudo rm -fr .git
+  mv druparcheky_subtheme.info.yml.example druparcheky_subtheme.info.yml
+  rename "s/^druparcheky_subtheme/$THEME_NAME/" druparcheky_subtheme*
+  find ${NEW_DIR} -type f -print0 | xargs -0 sed -i "s/druparcheky_subtheme/$THEME_NAME/g"
+  rm generate-subtheme.sh
 fi
-
